187e5ad34344e736578ca13db7df2591dd7e824e
iamhridoy11 <m_rahman_hridoy@yahoo.com>
2023-10-03 17:35:05 -0400

    Push

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template,  redirect, flash, session
+# from flask_debugtoolbar import DebugToolbarExtension
+from models import db,  connect_db, Department, Employee, get_directory
+
+app = Flask(__name__)
+app.app_context().push()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///employees_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+# app.config['SECRET_KEY'] = "chickenzarecool21837"
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+# debug = DebugToolbarExtension(app)
+
+connect_db(app)
+
+
+@app.route('/phones')
+def list_phones():
+    emps = Employee.query.all()
+    return render_template('phone.html', emps=emps)
---
+++ models.py
@@ -0,0 +1,54 @@
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+
+    db.app = app
+    db.init_app(app)
+
+
+# Models go below!
+
+class Department(db.Model):
+    """Department Model"""
+
+    __tablename__ = "departments"
+
+    dept_code = db.Column(db.Text, primary_key=True)
+    dept_name = db.Column(db.Text, nullable=False, unique=True)
+    phone = db.Column(db.Text)
+
+    # Used backref.
+    # employees = db.relationship("Employee")
+
+    def __repr__(self):
+        return f"<Department {self.dept_code} {self.dept_name} {self.phone}>"
+
+
+class Employee(db.Model):
+    """Employee Model"""
+
+    __tablename__ = "employees"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+    state = db.Column(db.Text, nullable=False, default='CA')
+    dept_code = db.Column(db.Text, db.ForeignKey('departments.dept_code'))
+
+    dept = db.relationship("Department", backref="employees")
+
+    def __repr__(self):
+        return f"<Employee {self.name} {self.state} {self.dept_code}>"
+
+
+def get_directory():
+    all_emps = Employee.query.all()
+
+    for emp in all_emps:
+        if emp.dept is not None:
+            print(emp.name, emp.dept.dept_name, emp.dept.phone)
+        else:
+            print(emp.name)
